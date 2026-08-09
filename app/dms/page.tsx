66b43d1e0c4446287aa4ab1cf2cf6798ab76a8a2
incredibean-hash <incredibean@gmail.com>
2026-08-09 16:21:47 -0500

    Delete app directory

--- app/dms/page.tsx
+++
@@ -1,4 +0,0 @@
-export const dynamic = 'force-dynamic';
-export default function DmsPage() {
-  return <div style={{ minHeight: "100vh", background: "black", color: "white", padding: "20px" }}><h1>DMs</h1></div>;
-}
