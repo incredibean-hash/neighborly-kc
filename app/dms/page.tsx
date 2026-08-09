ff5d2ea6db2cef9a78e9a25936cdcb4374c87bfb
incredibean-hash <incredibean@gmail.com>
2026-08-09 13:54:33 -0500

    Update page.tsx

--- app/dms/page.tsx
+++ app/dms/page.tsx
@@ -1,10 +1,4 @@
 export const dynamic = 'force-dynamic';
-
 export default function DmsPage() {
-  return (
-    <div style={{ minHeight: "100vh", background: "black", color: "white", padding: "20px" }}>
-      <h1>DMs</h1>
-      <p style={{ opacity: 0.5, marginTop: "20px" }}>Messages will show here. Build bypass active.</p>
-    </div>
-  );
+  return <div style={{ minHeight: "100vh", background: "black", color: "white", padding: "20px" }}><h1>DMs</h1><p>Build bypass active</p></div>
 }
