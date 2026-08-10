6c7d16b9734f2f91f6b746ae4fb4ae083fcb5d2a
incredibean-hash <incredibean@gmail.com>
2026-08-09 23:29:19 -0500

    Delete app directory

--- app/layout.tsx
+++
@@ -1,7 +0,0 @@
-
-import type { Metadata } from "next";
-import "./globals.css";
-export const metadata: Metadata = { title: "Neighborly KC", description: "Neighborly KC - 5mi - 304 NE 115TH ST" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="en"><body>{children}</body></html>;
-}
