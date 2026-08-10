5d6d04681dac771b82687d90d637eeb2d0eef0c1
incredibean-hash <incredibean@gmail.com>
2026-08-09 22:42:09 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,3 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { images: { domains: ['*.supabase.co'] }, experimental: { missingSuspenseWithCSRBailout: false } };
-module.exports = nextConfig;
