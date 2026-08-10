7824911a9b54e98de6441895dcff75a3f73a1279
incredibean-hash <incredibean@gmail.com>
2026-08-10 00:23:13 -0500

    Delete app directory

--- app/layout.tsx
+++
@@ -1,6 +0,0 @@
-
-import './globals.css';
-export const metadata={title:'Neighborly KC - Nextdoor Clone', description:'Kansas City neighborhood network - 40 mile radius'};
-export default function RootLayout({children}:{children:React.ReactNode}){
-  return (<html lang="en"><body>{children}</body></html>);
-}
